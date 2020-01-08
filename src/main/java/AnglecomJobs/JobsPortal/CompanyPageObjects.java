25299aa8c962b00ac9961e1c07c1bceba4e56ecc
vidya151992 <59662890+vidya151992@users.noreply.github.com>
2020-01-08 23:52:30 +0530

    Update CompanyPageObjects.java

--- src/main/java/AnglecomJobs/JobsPortal/CompanyPageObjects.java
+++ src/main/java/AnglecomJobs/JobsPortal/CompanyPageObjects.java
@@ -1,5 +1,16 @@
 package AnglecomJobs.JobsPortal;
 
 public class CompanyPageObjects {
+  @FindBy(how = How.XPATH , using = "//div[@class='jobList_9cc29']/div/div/a/div[1]/h6")
+	public  List<WebElement> jobType;
+	
+	@FindBy(how = How.TAG_NAME , using = "h4")
+	public  List<WebElement> jobTitle;
+	
+	@FindBy(how = How.XPATH , using = "//div[@class='jobList_9cc29']/div/div/div[1]/div/div/span")
+	public  List<WebElement> jobLocation;*/
+	
+	private  String xpathOfCompanyNames = "//a[contains(text(),'companyName')]";
+	
 
 }
